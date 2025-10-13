1d988d4734852b51a851bcbc621f061eaa0fd5ca
yani0514 <yanislv1405@mail.bg>
2025-10-13 16:01:38 +0200

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Created App component with basic state management and Tailwind styling.
    - Set up index.css to import Tailwind CSS.
    - Configured main entry point for React application.
    - Added Vite configuration to integrate React and Tailwind CSS plugins.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  console.log("dndandawd")
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Tailwind OK</h1>
+    </>
+  )
+}
+
+export default App
